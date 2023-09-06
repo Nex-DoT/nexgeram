e0fcfc6d9b5f2d8cf205ed0da1679cbf8d498107
NEX-DoT <nex.dot2@gmail.com>
2023-09-06 16:03:27 +0330

    pv action added

--- src/component/User/Chats/PvComponent/Pv.js
+++ src/component/User/Chats/PvComponent/Pv.js
@@ -12,14 +12,14 @@ const inistialState = {
         textColor: false,
     },
     color:{
-        Orient:true,      // "#005e7c"
+        Orient:true,       // "#005e7c"
         BlackPearl:false,  // "#040f16"
         PacificBlue:false, // "#0094c6"
         Stratos: false,    // "#001242"
         BlackRussian: false// "#000022"
     },
     textSize:{
-        sx:true,
+        xs:true,
         sm:false,
         md:false,
         lg:false,
@@ -30,6 +30,7 @@ const inistialState = {
     }
 
 }
+//funtion for reducer
 const functionReducer = (state , action)=>{
     switch(action.type){
         case "BACKCOLOR": 
@@ -49,7 +50,7 @@ const functionReducer = (state , action)=>{
             }
         case "TEXTSIZE": 
             let textSizeReset = {
-                sx:false,
+                xs:false,
                 sm:false,
                 md:false,
                 lg:false,
@@ -86,6 +87,7 @@ const functionReducer = (state , action)=>{
                     [action.Name] : true
                 }
             }
+        default : return state
     }
 }
 //component
@@ -124,35 +126,35 @@ const Pv = ({Data}) => {
             </div>
             {more && <div className=' shadow-lg absolute right-2 top-16 w-28 h-28 rounded-md bg-gray-500'>
                     <ul className='w-full h-full flex flex-col justify-around items-center'>
-                        <li className='h-1/3 justify-center items-center flex text-white hover:bg-gray-600 w-full rounded-t-md cursor-pointer text-xs'>background color</li>
-                        <li className='h-1/3 w-full border-gray-400 border-b border-t flex items-center justify-center text-white hover:bg-gray-600 cursor-pointer text-xs'>font size</li>
-                        <li className='h-1/3 flex items-center justify-center text-white hover:bg-gray-600 w-full rounded-b-md cursor-pointer text-xs'>text color</li>
+                        <li onMouseOver={()=>{action({type:"OPENSTYLE" , Name:"color"})}} className='h-1/3 justify-center items-center flex text-white hover:bg-gray-600 w-full rounded-t-md cursor-pointer text-xs'>background color</li>
+                        <li onMouseOver={()=>{action({type:"OPENSTYLE" , Name:"textSize"})}} className='h-1/3 w-full border-gray-400 border-b border-t flex items-center justify-center text-white hover:bg-gray-600 cursor-pointer text-xs'>font size</li>
+                        <li onMouseOver={()=>{action({type:"OPENSTYLE" , Name:"textColor"})}} className='h-1/3 flex items-center justify-center text-white hover:bg-gray-600 w-full rounded-b-md cursor-pointer text-xs'>text color</li>
                                 {state.menushow.color &&
                                     <div className='w-16 h-28 absolute right-32 felx items-center justify-center rounded-md bg-gray-500 z-20'>
                                         <ul className='flex flex-wrap items-center justify-around h-full relative after:absolute  after:bg-gray-500 after:w-4 after:h-4 after:top-2 after:-right-2 after:z-10 after:rotate-45 '>
-                                            <li className=' w-6 h-6 rounded-full z-20 transition-all cursor-pointer hover:scale-105 bg-orient border'></li>
-                                            <li className=' w-6 h-6 rounded-full z-20 transition-all cursor-pointer hover:scale-105 bg-blackpearl border'></li>
-                                            <li className=' w-6 h-6 rounded-full z-20 transition-all cursor-pointer hover:scale-105 bg-pacificblue  border'></li>
-                                            <li className=' w-6 h-6 rounded-full z-20 transition-all cursor-pointer hover:scale-105 bg-blackrussian border'></li>
-                                            <li className=' w-6 h-6 rounded-full z-20 transition-all cursor-pointer hover:scale-105 bg-stratos  border'></li>
+                                            <li onClick={()=>{action({type:"BACKCOLOR" , Name:"Orient"})}} className=' w-6 h-6 rounded-full z-20 transition-all cursor-pointer hover:scale-105 bg-orient border'></li>
+                                            <li onClick={()=>{action({type:"BACKCOLOR" , Name:"BlackPearl"})}} className=' w-6 h-6 rounded-full z-20 transition-all cursor-pointer hover:scale-105 bg-blackpearl border'></li>
+                                            <li onClick={()=>{action({type:"BACKCOLOR" , Name:"PacificBlue"})}} className=' w-6 h-6 rounded-full z-20 transition-all cursor-pointer hover:scale-105 bg-pacificblue  border'></li>
+                                            <li onClick={()=>{action({type:"BACKCOLOR" , Name:"BlackRussian"})}} className=' w-6 h-6 rounded-full z-20 transition-all cursor-pointer hover:scale-105 bg-blackrussian border'></li>
+                                            <li onClick={()=>{action({type:"BACKCOLOR" , Name:"Stratos"})}} className=' w-6 h-6 rounded-full z-20 transition-all cursor-pointer hover:scale-105 bg-stratos  border'></li>
                                         </ul>
                                     </div>
                                      }
                                 {state.menushow.textSize &&
                                     <div className='w-16 h-28 absolute right-32 felx items-center justify-center rounded-md bg-gray-500 z-20'>
                                         <ul className='flex flex-col items-center justify-between h-full relative after:absolute  after:bg-gray-500 after:w-4 after:h-4 after:top-12 after:-right-2 after:z-10 after:rotate-45 '>
-                                            <li className='text-sx text-white w-full h-full hover:bg-gray-600'>Small*2</li>
-                                            <li className='text-sx text-white w-full h-full hover:bg-gray-600'>Small</li>
-                                            <li className='text-sx text-white w-full h-full hover:bg-gray-600'>Mediom</li>
-                                            <li className='text-sx text-white w-full h-full hover:bg-gray-600'>Large</li>
+                                            <li onClick={()=>{action({type:"TEXTSIZE" , Name:"xs"})}} className='z-20 text-xs cursor-pointer text-white w-full h-full hover:bg-gray-600 flex items-center justify-center rounded-t-md'>Small*2</li>
+                                            <li onClick={()=>{action({type:"TEXTSIZE" , Name:"sm"})}} className='z-20 text-xs cursor-pointer text-white w-full h-full hover:bg-gray-600 flex items-center justify-center'>Small</li>
+                                            <li onClick={()=>{action({type:"TEXTSIZE" , Name:"md"})}} className='z-20 text-xs cursor-pointer text-white w-full h-full hover:bg-gray-600 flex items-center justify-center'>Mediom</li>
+                                            <li onClick={()=>{action({type:"TEXTSIZE" , Name:"lg"})}} className='z-20 text-xs cursor-pointer text-white w-full h-full hover:bg-gray-600 flex items-center justify-center rounded-b-md'>Large</li>
                                         </ul>
                                     </div>
                                     }
                                 {state.menushow.textColor &&
                                     <div className='w-16 h-28 absolute right-32 felx items-center justify-center rounded-md bg-gray-500 z-20'>
-                                        <ul className='flex flex-col items-center justify-between h-full relative after:absolute  after:bg-gray-500 after:w-4 after:h-4 after:top-12 after:-right-2 after:z-10 after:rotate-45 '>
-                                            <li className='text-sx text-white w-full h-full hover:bg-gray-600'>White</li>
-                                            <li className='text-sx text-white w-full h-full hover:bg-gray-600'>Black</li>
+                                        <ul className='flex flex-col items-center w-full justify-between h-full relative after:absolute  after:bg-gray-500 after:w-4 after:h-4 after:top-20 after:-right-2 after:z-10 after:rotate-45 '>
+                                            <li onClick={()=>{action({type:"TEXTCOLER" , Name:"white"})}} className=' text-xs text-white w-full h-full hover:bg-gray-600 rounded-t-md flex items-center justify-center cursor-pointer z-20'>White</li>
+                                            <li onClick={()=>{action({type:"TEXTCOLER" , Name:"black"})}} className='text-xs text-white w-full h-full hover:bg-gray-600 rounded-b-md flex items-center justify-center cursor-pointer z-20'>Black</li>
                                         </ul>
                                     </div>
                                      }
